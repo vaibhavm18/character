67574fdd80f18f749b15479ac1bdb45acdc76b01
vaibhav <vaibhavnayak018@gmail.com>
2024-09-18 19:10:59 +0530

    adding enter button

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -3,10 +3,10 @@ import { Input } from "../ui/input";
 import { SendHorizontal, Phone } from "lucide-react";
 
 interface Props {
-  appendText: (text:string) => void
+  appendText: (text: string) => void;
 }
 
-export default function MessageInput({appendText}: Props) {
+export default function MessageInput({ appendText }: Props) {
   const [inputText, setInputText] = useState("");
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -14,9 +14,18 @@ export default function MessageInput({appendText}: Props) {
   };
 
   const onSubmit = () => {
-    appendText(inputText)
-    setInputText("")
-  }
+    if (inputText.trim() !== "") {
+      appendText(inputText);
+      setInputText("");
+    }
+  };
+
+  const handlePressKey = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      onSubmit();
+    }
+  };
 
   return (
     <div className="w-full flex items-center gap-2 py-6 px-2 max-w-3xl mx-auto">
@@ -24,10 +33,14 @@ export default function MessageInput({appendText}: Props) {
         <Input
           onChange={onChange}
           value={inputText}
+          onKeyDown={handlePressKey}
           className="rounded-3xl h-12 text-white bg-[#202024] border-none"
           placeholder="Message username"
         />
-        <span onClick={onSubmit} className="p-2 absolute bg-[#FAFAFA] text-black rounded-full right-2 top-1 ">
+        <span
+          onClick={onSubmit}
+          className="p-2 absolute bg-[#FAFAFA] text-black rounded-full right-2 top-1 "
+        >
           <SendHorizontal />
         </span>
       </div>
